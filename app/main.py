b534fd5f3d317880e9fe8a0dbc03a59258c9fd41
amandev-x <amandabral2104@gmail.com>
2026-03-05 09:16:20 +0530

    removed crash logic code

--- app/main.py
+++ app/main.py
@@ -11,10 +11,6 @@ ENVIRONMENT = os.environ.get("ENVIRONMENT", "unknown")
 VERSION = os.environ.get("VERSION", "1.0.0")
 FAIL_HEALTH = os.environ.get("FAIL_HEALTH", "false").lower() == "true"
 
-@app.on_event("startup")
-def crash():
-    raise Exception("Intentional failure for GitOps testing")
-
 @app.get("/")
 async def read_root():
     return {
